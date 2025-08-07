baa4e2f84e02380c167756a4b7e827a2561afdee
MiraHikari <69497500+MiraHikari@users.noreply.github.com>
2025-08-08 05:11:39 +0800

    ignore sth.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // ignore lint
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
